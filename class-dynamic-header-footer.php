005d888cd24454852249b317a5626d9c7f30feab
nikhilc@bsf.io <nikhilc@bsf.io>
2016-09-17 13:42:20 +0530

    remove adding option to use page template on pages manually

--- class-dynamic-header-footer.php
+++ class-dynamic-header-footer.php
@@ -14,21 +14,13 @@ class Dynamic_Header_Footer {
 	function __construct() {
 
 		$this->supported_themes = array(
-				'bb-theme'
-			);
+			'bb-theme'
+		);
 
 		$this->set_template_path();
 
 		// Check where we want to force the page template
 		$this->check_forced_template();
-
-		// Add a filter to the attributes metabox to inject template into the cache.
-		add_filter( 'page_attributes_dropdown_pages_args', array( $this, 'dhf_register_project_templates' ) );
-
-		// Add a filter to the save post to inject out template into the page cache
-		add_filter( 'wp_insert_post_data', array( $this, 'dhf_register_project_templates' ) );
-
-		add_filter( 'template_include', array( $this, 'dhf_view_project_template' ) );
 
 		// Scripts and styles
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
@@ -63,53 +55,14 @@ class Dynamic_Header_Footer {
 		return $page_template;
 	}
 
-	function dhf_register_project_templates( $atts ) {
-
-		// Create the key used for the themes cache
-		$cache_key = 'page_templates-' . md5( get_theme_root() . '/' . get_stylesheet() );
-
-		$templates = wp_get_theme()->get_page_templates();
-		if ( empty( $templates ) ) {
-			$templates = array();
-		}
-
-		wp_cache_delete( $cache_key, 'themes' );
-
-		$templates = array_merge( $templates, $this->templates );
-		wp_cache_add( $cache_key, $templates, 'themes', 1800 );
-
-		return $atts;
-	}
-
-	function dhf_view_project_template( $template ) {
-
-		global $post;
-
-		if ( ! isset( $this->templates[ get_post_meta( $post->ID, '_wp_page_template', true ) ] ) ) {
-
-			return $template;
-		}
-
-		$file = DHF_DIR . '/templates/default/' . get_post_meta( $post->ID, '_wp_page_template', true );
-
-		// Just to be safe, we check if the file exist first
-		if ( file_exists( $file ) ) {
-			return $file;
-		} else {
-			echo $file;
-		}
-
-		return $template;
-	}
-
 	public function set_template_path() {
 
 		$template = get_template();
-		
+
 		if ( in_array( $template, $this->supported_themes ) ) {
-						
-			$this->template_file = DHF_DIR . 'templates/'. $template .'/template-page-builder.php';
-			$this->template_dir = DHF_DIR . 'templates/'. $template .'';
+
+			$this->template_file = DHF_DIR . 'templates/' . $template . '/template-page-builder.php';
+			$this->template_dir  = DHF_DIR . 'templates/' . $template . '';
 
 			$this->templates = array(
 				'template-page-builder.php' => 'Page Builder Template'
@@ -117,13 +70,13 @@ class Dynamic_Header_Footer {
 		} else {
 
 			$this->template_file = DHF_DIR . 'templates/default/template-page-builder.php';
-			$this->template_dir = DHF_DIR . 'templates/default';
+			$this->template_dir  = DHF_DIR . 'templates/default';
 
 			$this->templates = array(
 				'template-page-builder.php' => 'Page Builder Template'
 			);
 		}
-		
+
 	}
 
 	public function get_header() {
@@ -134,7 +87,7 @@ class Dynamic_Header_Footer {
 			load_template( $this->template_dir . '/header.php' );
 		} else {
 			get_header();
-		}		
+		}
 	}
 
 	public function get_footer() {
@@ -151,14 +104,14 @@ class Dynamic_Header_Footer {
 	public static function get_header_content() {
 
 		$header_id = Dynamic_Header_Footer::get_settings( 'dhf_header_id', '' );
-		echo do_shortcode( '[fl_builder_insert_layout id="' .$header_id. '"]' );
+		echo do_shortcode( '[fl_builder_insert_layout id="' . $header_id . '"]' );
 	}
 
 	public static function get_footer_content() {
 
 		$footer_id = Dynamic_Header_Footer::get_settings( 'dhf_footer_id', '' );
 		echo "<div class='footer-width-fixer'>";
-		echo do_shortcode( '[fl_builder_insert_layout id="' .$footer_id. '"]' );
+		echo do_shortcode( '[fl_builder_insert_layout id="' . $footer_id . '"]' );
 		echo "</div>";
 	}
 
