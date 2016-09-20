7313a56ca4720664e5992bc61aa7d2e038323793
nikhilc@bsf.io <nikhilc@bsf.io>
2016-09-20 22:58:31 +0530

    load class on plugins_loaded

--- bb-header-footer.php
+++ bb-header-footer.php
@@ -20,4 +20,8 @@ define( 'BBHF_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BBHF_URL', plugins_url( '/', __FILE__ ) );
 define( 'BBHF_PATH', plugin_basename( __FILE__ ) );
 
-new BB_Header_Footer();
+function bb_header_footer_init() {
+	new BB_Header_Footer();	
+}
+
+add_action( 'plugins_loaded', 'bb_header_footer_init' );
