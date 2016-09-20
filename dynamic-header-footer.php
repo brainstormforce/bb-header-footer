3a729f71540ffa1ba6b8e33da83a5f18c29c8771
nikhilc@bsf.io <nikhilc@bsf.io>
2016-09-20 09:57:53 +0530

    version bump to v0.2

--- dynamic-header-footer.php
+++ dynamic-header-footer.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     dynamic-header-footer
  * Domain Path:     /languages
- * Version:         0.1
+ * Version:         0.2
  *
  * @package         Dynamic_Header_Footer
  */
@@ -15,7 +15,7 @@
 require_once 'class-dynamic-header-footer.php';
 require_once 'class-dhf-admin-ui.php';
 
-define( 'DHF_VER', '0.1' );
+define( 'DHF_VER', '0.2' );
 define( 'DHF_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DHF_URL', plugins_url( '/', __FILE__ ) );
 define( 'DHF_PATH', plugin_basename( __FILE__ ) );
