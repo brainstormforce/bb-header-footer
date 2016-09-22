791c3b21e64ac70ed0abb1f72a17a3261ec99c39
nikhilc@bsf.io <nikhilc@bsf.io>
2016-09-22 19:51:50 +0530

    Improvement: preserve header and footer schema for bb-theme

--- themes/bb-theme/class-bb-theme-compat.php
+++ themes/bb-theme/class-bb-theme-compat.php
@@ -30,12 +30,12 @@ class BB_Theme_Compat {
 
 		if ( $header_id !== '' ) {
 			add_filter( 'fl_header_enabled', array( $this, 'disable_bb_theme_header' ) );
-			add_action( 'fl_before_header', array( 'BB_Header_Footer', 'get_header_content' ) );
+			add_action( 'fl_before_header', array( $this, 'get_header_content' ) );
 		}
 
 		if ( $footer_id !== '' ) {
 			add_filter( 'fl_footer_enabled', array( $this, 'disable_bb_theme_footer' ) );
-			add_action( 'fl_after_content', array( 'BB_Header_Footer', 'get_footer_content' ) );
+			add_action( 'fl_after_content', array( $this, 'get_footer_content' ) );
 		}
 
 	}
@@ -46,6 +46,22 @@ class BB_Theme_Compat {
 
 	public function disable_bb_theme_footer() {
 		return false;
+	}
+
+	public function get_header_content() {
+		?>
+			<header temscope="itemscope" itemtype="http://schema.org/WPHeader">
+				<?php BB_Header_Footer::get_header_content(); ?>
+			</header>
+		<?php
+	}
+
+	public function get_footer_content() {
+		?>
+			<footer itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+				<?php BB_Header_Footer::get_footer_content(); ?>
+			</footer>
+		<?php
 	}
  
 }
