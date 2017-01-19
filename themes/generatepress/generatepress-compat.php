4cafb02002449ede49042d57e763516ea5cf9206
Nikhil Chavan <email@nikhilchavan.com>
2017-01-19 23:40:10 +0530

    add function docs for generatepress compat functions

--- themes/generatepress/generatepress-compat.php
+++ themes/generatepress/generatepress-compat.php
@@ -1,12 +1,20 @@
 <?php
+/**
+ * Generatepress Theme Compatibility.
+ *
+ * @package  bb-header-footer
+ */
 
 if ( ! function_exists( 'generate_header_items' ) ) {
 
+	/**
+	 * Override generatepress function for overriding header content.
+	 */
 	function generate_header_items() {
 
 		$header_id = BB_Header_Footer::get_settings( 'bb_header_id', '' );
 
-		if ( $header_id !== '' ) {
+		if ( '' !== $header_id ) {
 			?>
 
 			<p class="main-title bhf-hidden" itemprop="headline"><a href="<?php echo bloginfo( 'url' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
@@ -15,24 +23,29 @@ if ( ! function_exists( 'generate_header_items' ) ) {
 			<?php
 		} else {
 
-			// Header widget
+			// Header widget.
 			generate_construct_header_widget();
 
-			// Site title and tagline
+			// Site title and tagline.
 			generate_construct_site_title();
 
-			// Site logo
+			// Site logo.
 			generate_construct_logo();
 		}
 	}
 }
 
-
+/**
+ * Override number of active widgets
+ *
+ * @param  int $widgets Number of active widgets.
+ * @return int          Returns 0 if footer is selected in the plugin options to disable the footer widgets.
+ */
 function bb_generate_footer_widgets_override( $widgets ) {
 
 	$footer_id = BB_Header_Footer::get_settings( 'bb_footer_id', '' );
 
-	if ( $footer_id !== '' ) {
+	if ( '' !== $footer_id ) {
 
 		return 0;
 	} else {
@@ -43,11 +56,14 @@ function bb_generate_footer_widgets_override( $widgets ) {
 
 add_filter( 'generate_footer_widgets', 'bb_generate_footer_widgets_override' );
 
+/**
+ * Change content of footer section in generatepress.
+ */
 function bb_generate_add_footer() {
 
 	$footer_id = BB_Header_Footer::get_settings( 'bb_footer_id', '' );
 
-	if ( $footer_id !== '' ) {
+	if ( '' !== $footer_id ) {
 
 		BB_Header_Footer::get_footer_content();
 	}
