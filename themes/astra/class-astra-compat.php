2dbf56d4fe8ad14b2f1de17c26a9e33c77617d4d
Nikhil Chavan <email@nikhilchavan.com>
2017-06-04 21:04:35 +0530

    Add header and footer html tags when adding header and footer from the plugin

--- themes/astra/class-astra-compat.php
+++ themes/astra/class-astra-compat.php
@@ -41,12 +41,12 @@ class Astra_Compat {
 
 		if ( '' !== $header_id ) {
 			add_action( 'template_redirect', array( $this, 'astra_setup_header' ), 10 );
-			add_action( 'astra_header', array( 'BB_Header_Footer', 'get_header_content' ), 16 );
+			add_action( 'astra_header', array( $this, 'add_header_markup' ) );
 		}
 
 		if ( '' !== $footer_id ) {
 			add_action( 'template_redirect', array( $this, 'astra_setup_footer' ), 10 );
-			add_action( 'astra_footer', array( 'BB_Header_Footer', 'get_footer_content' ), 16 );
+			add_action( 'astra_footer', array( $this, 'add_footer_markup' ) );
 		}
 
 	}
@@ -59,10 +59,35 @@ class Astra_Compat {
 	}
 
 	/**
+	 * Display header markup.
+	 */
+	public function add_header_markup() {
+		?>
+			<header id="masthead" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
+				<p class="main-title bhf-hidden" itemprop="headline"><a href="<?php echo bloginfo( 'url' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php BB_Header_Footer::get_header_content(); ?>
+			</header>
+
+		<?php
+	}
+
+	/**
 	 * Disable footer from the theme.
 	 */
 	public function astra_setup_footer() {
 		remove_action( 'astra_footer', 'astra_footer_markup' );
+	}
+
+	/**
+	 * Display footer markup.
+	 */
+	public function add_footer_markup() {
+
+		?>
+			<footer itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+				<?php BB_Header_Footer::get_footer_content(); ?>
+			</footer>
+		<?php
 	}
 
 
