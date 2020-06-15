5e270b759351bd6f162bd483aca45ab6e2e0ab41
leenam <leenam@bsf.io>
2020-06-15 14:41:28 +0530

    updated bsf analytics

--- admin/bsf-analytics/class-bsf-analytics.php
+++ admin/bsf-analytics/class-bsf-analytics.php
@@ -33,7 +33,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		public function __construct() {
 
 			define( 'BSF_ANALYTICS_FILE', __FILE__ );
-			define( 'BSF_ANALYTICS_VERSION', '1.0.0' );
+			define( 'BSF_ANALYTICS_VERSION', '1.0.1' );
 			define( 'BSF_ANALYTICS_PATH', dirname( __FILE__ ) );
 			define( 'BSF_ANALYTICS_URI', $this->bsf_analytics_url() );
 
@@ -180,11 +180,13 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			}
 
 			/* translators: %s product name */
-			$notice_string = __( 'Want to help make <strong>%1s</strong> even more awesome? Allow us to collect non-sensitive diagnostic data and usage information. ', 'astra' );
+			$notice_string = __( 'Want to help make <strong>%1s</strong> even more awesome? Allow us to collect non-sensitive diagnostic data and usage information. ', 'bb-header-footer' );
 
 			if ( is_multisite() ) {
-				$notice_string .= __( 'This will be applicable for all sites from the network.', 'astra' );
+				$notice_string .= __( 'This will be applicable for all sites from the network.', 'bb-header-footer' );
 			}
+
+			$language_dir = is_rtl() ? 'rtl' : 'ltr';
 
 			Astra_Notices::add_notice(
 				array(
@@ -205,14 +207,14 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 								</div>
 							</div>',
 						/* translators: %s usage doc link */
-						sprintf( $notice_string . '<a href="%2s" target="_blank" rel="noreferrer noopener">%3s</a>', $this->get_product_name(), esc_url( $this->usage_doc_link ), __( ' Know More.', 'astra' ) ),
+						sprintf( $notice_string . '<span dir="%2s"><a href="%3s" target="_blank" rel="noreferrer noopener">%4s</a><span>', esc_html( $this->get_product_name() ), $language_dir, esc_url( $this->usage_doc_link ), __( ' Know More.', 'bb-header-footer' ) ),
 						add_query_arg(
 							array(
 								'bsf_analytics_optin' => 'yes',
 								'bsf_analytics_nonce' => wp_create_nonce( 'bsf_analytics_optin' ),
 							)
 						),
-						__( 'Yes! Allow it', 'astra' ),
+						__( 'Yes! Allow it', 'bb-header-footer' ),
 						add_query_arg(
 							array(
 								'bsf_analytics_optin' => 'no',
@@ -220,7 +222,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 							)
 						),
 						MONTH_IN_SECONDS,
-						__( 'No Thanks', 'astra' )
+						__( 'No Thanks', 'bb-header-footer' )
 					),
 					'show_if'                    => true,
 					'repeat-notice-after'        => false,
@@ -240,11 +242,11 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 				return;
 			}
 
-			if ( ! wp_verify_nonce( sanitize_text_field( $_GET['bsf_analytics_nonce'] ), 'bsf_analytics_optin' ) ) {
+			if ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_GET['bsf_analytics_nonce'] ) ), 'bsf_analytics_optin' ) ) {
 				return;
 			}
 
-			$optin_status = sanitize_text_field( $_GET['bsf_analytics_optin'] );
+			$optin_status = isset( $_GET['bsf_analytics_optin'] ) ? sanitize_text_field( wp_unslash( $_GET['bsf_analytics_optin'] ) ) : '';
 
 			if ( 'yes' === $optin_status ) {
 				$this->optin();
@@ -289,7 +291,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		public function every_two_days_schedule( $schedules ) {
 			$schedules['every_two_days'] = array(
 				'interval' => 2 * DAY_IN_SECONDS,
-				'display'  => __( 'Every two days', 'astra' ),
+				'display'  => __( 'Every two days', 'bb-header-footer' ),
 			);
 
 			return $schedules;
@@ -343,7 +345,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 
 			add_settings_field(
 				'bsf-analytics-optin',       // Field ID.
-				__( 'Usage Tracking', 'astra' ),       // Field title.
+				__( 'Usage Tracking', 'bb-header-footer' ),       // Field title.
 				array( $this, 'render_settings_field_html' ), // Field callback function.
 				'general'                    // Settings page slug.
 			);
@@ -371,18 +373,22 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		 */
 		public function render_settings_field_html() {
 			?>
+			<fieldset>
 			<label for="bsf-analytics-optin">
 				<input id="bsf-analytics-optin" type="checkbox" value="1" name="bsf_analytics_optin" <?php checked( get_site_option( 'bsf_analytics_optin', 'no' ), 'yes' ); ?>>
 				<?php
-				esc_html_e( 'Allow Brainstorm Force products to track non-sensitive usage tracking data.', 'astra' );
+				esc_html_e( 'Allow Brainstorm Force products to track non-sensitive usage tracking data.', 'bb-header-footer' );
 
 				if ( is_multisite() ) {
-					esc_html_e( ' This will be applicable for all sites from the network.', 'astra' );
+					esc_html_e( ' This will be applicable for all sites from the network.', 'bb-header-footer' );
 				}
 				?>
 			</label>
 			<?php
-			echo wp_kses_post( sprintf( '<a href="%1s" target="_blank" rel="noreferrer noopener">%2s</a>', esc_url( $this->usage_doc_link ), __( 'Learn More.', 'astra' ) ) );
+			echo wp_kses_post( sprintf( '<a href="%1s" target="_blank" rel="noreferrer noopener">%2s</a>', esc_url( $this->usage_doc_link ), __( 'Learn More.', 'bb-header-footer' ) ) );
+			?>
+			</fieldset>
+			<?php
 		}
 
 		/**
@@ -406,14 +412,30 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			$exploded_path = explode( '/', $base, 2 );
 			$plugin_slug   = $exploded_path[0];
 
+			return $this->get_plugin_name( $plugin_slug );
+		}
+
+		/**
+		 * Get plugin name by plugin slug.
+		 *
+		 * @param string $plugin_slug Plugin slug.
+		 * @return string $plugin_info['Name'] Plugin name.
+		 */
+		private function get_plugin_name( $plugin_slug ) {
+
+			$plugins = get_option( 'active_plugins' );
+
 			if ( ! function_exists( 'get_plugin_data' ) ) {
 				require_once ABSPATH . 'wp-admin/includes/plugin.php';
 			}
 
-			$plugin_main_file = WP_PLUGIN_DIR . '/' . $plugin_slug . '/' . $plugin_slug . '.php';
-			$plugin_data      = get_plugin_data( wp_normalize_path( $plugin_main_file ) );
-
-			return $plugin_data['Name'];
+			foreach ( $plugins as $plugin_file ) {
+				if ( 0 === strpos( $plugin_file, $plugin_slug ) ) {
+					$plugin_path = WP_PLUGIN_DIR . '/' . $plugin_file;
+					$plugin_data = get_plugin_data( $plugin_path );
+					return $plugin_data['Name'];
+				}
+			}
 		}
 
 		/**
